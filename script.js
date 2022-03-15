a56b97ae37deba6967b41e57b851675154e1c88c
Benmuiruri <ben.muiruri13@gmail.com>
2022-03-15 18:28:51 +0300

    Add delete book, update ui and add book to ui methods

--- script.js
+++ script.js
@@ -37,4 +37,36 @@ class UpdateDisplay {
       UpdateDisplay.addBookItem(bookItem, books.length - 1);
     }
   }
+  static delBook(bookItem, pos) {
+    const bookBlock = document.getElementById(pos);
+    books = books.filter((item) => item !== bookItem);
+    localStorage.setItem('books', JSON.stringify(books));
+    UpdateDisplay.listSection.removeChild(bookBlock);
+  }
+  static updateUi() {
+    if (localStorage.getItem('books')) {
+      books = JSON.parse(localStorage.getItem('books'));
+      books.forEach((bookItem, pos) => {
+        UpdateDisplay.addBookItem(bookItem, pos);
+      });
+    } else {
+      localStorage.setItem('books', '');
+      books = [];
+    }
+  }
+  static addBookItem(bookItem, pos) {
+    const bookBlock = document.createElement('div');
+    bookBlock.classList.add('bookDIV');
+    bookBlock.id = pos;
+    const removeBtn = document.createElement('button');
+    removeBtn.classList.add('remove-btn');
+    bookBlock.innerHTML = `
+      <p class='book-title'>'${bookItem.author}'  by ${bookItem.title} </p>`;
+    removeBtn.innerText = 'Remove';
+    removeBtn.onclick = () => {
+      UpdateDisplay.delBook(bookItem, pos);
+    };
+    bookBlock.appendChild(removeBtn);
+    UpdateDisplay.listSection.appendChild(bookBlock);
+  }
 }
