3d8b8b512febc191e6c393d2bacc426fdfafc6f8
Benmuiruri <ben.muiruri13@gmail.com>
2022-03-16 16:26:51 +0300

    Refactor js to use es6 functions

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-// Initialize the books
-let books;
-
-// create the book class
-class UpdateDisplay {
+class Book {
   constructor(author, title) {
     this.title = title;
     this.author = author;
   }
+
+  static books = [];
 
   static listSection = document.querySelector('.book-list');
 
@@ -18,44 +16,40 @@ class UpdateDisplay {
 
   static listBtn = document.querySelectorAll('.listBtn');
 
-  static addActive= document.querySelectorAll('.section');
+  static addActive = document.querySelectorAll('.section');
 
-  // create new book
-  static addBooks() {
-    const bookItem = new UpdateDisplay(
-      UpdateDisplay.bookTitle.value,
-      UpdateDisplay.bookAuthor.value,
-    );
+  static addBooks = () => {
+    const bookItem = new Book(Book.bookTitle.value, Book.bookAuthor.value);
 
-    if (UpdateDisplay.bookTitle.value && UpdateDisplay.bookAuthor.value !== '') {
-      books.push(bookItem);
-      localStorage.setItem('books', JSON.stringify(books));
-      UpdateDisplay.bookAuthor.value = '';
-      UpdateDisplay.bookTitle.value = '';
-      UpdateDisplay.addBookItem(bookItem, books.length - 1);
+    if (Book.bookTitle.value && Book.bookAuthor.value !== '') {
+      Book.books.push(bookItem);
+      localStorage.setItem('bookInfo', JSON.stringify(Book.books));
+      Book.bookAuthor.value = '';
+      Book.bookTitle.value = '';
+      Book.addBookItem(bookItem, Book.books.length - 1);
     }
-  }
+  };
 
-  static delBook(bookItem, pos) {
+  static delBook = (bookItem, pos) => {
     const bookBlock = document.getElementById(pos);
-    books = books.filter((item) => item !== bookItem);
-    localStorage.setItem('books', JSON.stringify(books));
-    UpdateDisplay.listSection.removeChild(bookBlock);
-  }
+    Book.books = Book.books.filter((item) => item !== bookItem);
+    localStorage.setItem('bookInfo', JSON.stringify(Book.books));
+    Book.listSection.removeChild(bookBlock);
+  };
 
-  static updateUi() {
-    if (localStorage.getItem('books')) {
-      books = JSON.parse(localStorage.getItem('books'));
-      books.forEach((bookItem, pos) => {
-        UpdateDisplay.addBookItem(bookItem, pos);
+  static updateUi = () => {
+    if (localStorage.getItem('bookInfo')) {
+      Book.books = JSON.parse(localStorage.getItem('bookInfo'));
+      Book.books.forEach((bookItem, pos) => {
+        Book.addBookItem(bookItem, pos);
       });
     } else {
-      localStorage.setItem('books', '');
-      books = [];
+      localStorage.setItem('bookInfo', '');
+      Book.books = [];
     }
-  }
+  };
 
-  static addBookItem(bookItem, pos) {
+  static addBookItem = (bookItem, pos) => {
     const bookBlock = document.createElement('div');
     bookBlock.classList.add('bookDIV');
     bookBlock.id = pos;
@@ -65,15 +59,53 @@ class UpdateDisplay {
 
     bookBlock.innerHTML = `
       <p class='book-title'>'${bookItem.author}'  by ${bookItem.title} </p>`;
+
     removeBtn.innerText = 'Remove';
 
     removeBtn.onclick = () => {
-      UpdateDisplay.delBook(bookItem, pos);
+      Book.delBook(bookItem, pos);
     };
+
     bookBlock.appendChild(removeBtn);
-    UpdateDisplay.listSection.appendChild(bookBlock);
-  }
+    Book.listSection.appendChild(bookBlock);
+  };
 }
 
-UpdateDisplay.updateUi();
-UpdateDisplay.formBtn.addEventListener('click', UpdateDisplay.addBooks);
+Book.listBtn.forEach((btn, i) => {
+  btn.onclick = () => {
+    Book.listBtn.forEach((oldBtn) => {
+      oldBtn.classList.remove('active');
+    });
+    btn.classList.add('active');
+    Book.addActive.forEach((sec, index) => {
+      if (i === index) {
+        sec.classList.add('active');
+      } else {
+        sec.classList.remove('active');
+      }
+    });
+  };
+});
+
+Book.updateUi();
+
+Book.formBtn.addEventListener('click', Book.addBooks);
+
+const timeBox = document.querySelector('#date');
+
+function time() {
+  const date = new Date();
+  const locale = navigator.language;
+  const options = {
+    month: 'long',
+    day: 'numeric',
+    year: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
+    hour12: 'false',
+  };
+  timeBox.textContent = `${date.toLocaleTimeString(locale, options)}`;
+}
+
+setInterval(time, 1000);
